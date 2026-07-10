d38ca2790e2e862c459fb56958e18636f9c3d34b
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2026-07-09 20:34:00 -0400

    Fixed component

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
 
                         <article>
 
-                            <Countdown
+                            <CountDown
                                 targetDate="2026-07-23T00:00:00"
                             />
 
